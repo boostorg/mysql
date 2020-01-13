def1a8febc6c6677e5b6f6b5976ffeeb4d1c2c36
ruben <rubenperez038@gmail.com>
2020-01-13 22:39:51 +0000

    Doxygen comments for connection and resultset (1/2)

--- include/mysql/connection.hpp
+++ include/mysql/connection.hpp
@@ -11,15 +11,23 @@
 namespace mysql
 {
 
+/**
+ * \brief Parameters defining how to authenticate to a MySQL server.
+ */
 struct connection_params
 {
-	std::string_view username;
-	std::string_view password;
-	std::string_view database;
-	collation connection_collation;
+	std::string_view username; ///< Username to authenticate as.
+	std::string_view password; ///< Password for that username, possibly empty.
+	std::string_view database; ///< Database to use, or empty string for no database.
+	collation connection_collation; ///< The default character set and collation for the connection.
 
-	connection_params(std::string_view username, std::string_view password,
-			std::string_view db = "", collation connection_col = collation::utf8_general_ci) :
+	/// Initializing constructor
+	connection_params(
+		std::string_view username,
+		std::string_view password,
+		std::string_view db = "",
+		collation connection_col = collation::utf8_general_ci
+	) :
 		username(username),
 		password(password),
 		database(db),
@@ -28,7 +36,29 @@ struct connection_params
 	}
 };
 
-template <typename Stream>
+/**
+ * \brief A connection to a MySQL server.
+ * \details
+ * This is the basic object to instantiate to use the MySQL-Asio library.
+ *
+ * Connections allow you to interact with the database server (e.g. you
+ * can issue queries using connection::query).
+ *
+ * Before being able to use a connection, you must connect it. This encompasses two steps:
+ *   - Stream connection: you should make sure the Stream gets connected to
+ *     whatever endpoint the MySQL server lives on. You can access the underlying
+ *     Stream object by calling connection::next_layer(). For a TCP socket, this is equivalent
+ *     to connecting the socket.
+ *   - MySQL handshake: authenticates the connection to the MySQL server. You can do that
+ *     by calling connection::handshake() or connection::async_handshake().
+ *
+ * Because of how the MySQL protocol works, you must fully perform an operation before
+ * starting the next one. For queries, you must wait for the query response and
+ * **read the entire resultset** before starting a new query. Otherwise, results are undefined.
+ */
+template <
+	typename Stream ///< The underlying stream to use; must satisfy Boost.Asio's SyncStream and AsyncStream concepts.
+>
 class connection
 {
 	using channel_type = detail::channel<Stream>;
@@ -37,6 +67,10 @@ class connection
 	channel_type channel_;
 	detail::bytestring buffer_;
 public:
+	/**
+	 * \brief Initializing constructor.
+	 * \details Creates a Stream object by forwarding any passed in arguments to its constructor.
+	 */
 	template <typename... Args>
 	connection(Args&&... args) :
 		next_level_(std::forward<Args>(args)...),
@@ -44,27 +78,56 @@ public:
 	{
 	}
 
+	/// Retrieves the underlying Stream object.
 	Stream& next_level() { return next_level_; }
+
+	/// Retrieves the underlying Stream object.
 	const Stream& next_level() const { return next_level_; }
 
+	/// Performs the MySQL-level handshake (synchronous with error code version).
 	void handshake(const connection_params& params, error_code& ec);
+
+	/// Performs the MySQL-level handshake (synchronous with exceptions version).
 	void handshake(const connection_params& params);
 
+	/**
+	 * \brief Performs the MySQL-level handshake (asynchronous version).
+	 * \details The strings pointed to by params should be kept alive by the caller
+	 * until the operation completes, as no copy is made by the library.
+	 */
 	template <typename CompletionToken>
 	BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, void(error_code))
 	async_handshake(const connection_params& params, CompletionToken&& token);
 
+	/**
+	 * \brief Executes a SQL text query (sync with error code version).
+	 * \detail Does not perform the actual retrieval of the data; use the various
+	 * fetch functions within resultset to achieve that.
+	 *
+	 * Note that query_string may contain any valid SQL, not just SELECT statements.
+	 * If your query does not return any data, then the resultset will be empty
+	 * (\see resultset for more details).
+	 *
+	 * \warning After query() has returned, you should read the entire resultset
+	 * before calling query() again. Otherwise, the results are undefined.
+	 */
 	resultset<Stream> query(std::string_view query_string, error_code&);
+
+	/// Executes a SQL text query (sync with exceptions version).
 	resultset<Stream> query(std::string_view query_string);
 
+	/// Executes a SQL text query (async version).
 	template <typename CompletionToken>
 	BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, void(error_code, resultset<Stream>))
 	async_query(std::string_view query_string, CompletionToken&& token);
 };
 
+/// A connection to MySQL over TCP.
 using tcp_connection = connection<boost::asio::ip::tcp::socket>;
+
 // TODO: UNIX socket connection
 
+/// The default TCP port for the MySQL protocol.
 constexpr unsigned short default_port = 3306;
 
 }
--- include/mysql/resultset.hpp
+++ include/mysql/resultset.hpp
@@ -11,7 +11,41 @@
 namespace mysql
 {
 
-template <typename StreamType>
+/**
+ * \brief Represents tabular data retrieved from the MySQL server.
+ * \details Returned as the result of a query (\see connection::query).
+ *
+ * A resultset does not read all the retrieved information into memory
+ * directly. Instead, you use fetch_one, fetch_many
+ * or fetch_all to load rows progressively. This allows for better efficiency.
+ *
+ * A resultset can be in two states:
+ * - Complete, meaning that all data has already been retrieved for this
+ *   resultset.
+ * - Not complete, meaning that there is still rows to be read.
+ *
+ * You can test whether a resultset is complete or not by calling
+ * resultset::complete().
+ *
+ * Resultsets also contain metadata about the fields in the query.
+ * You can access them at any point using resultset::fields().
+ *
+ * The result of a SQL statement that does not return any data (e.g.
+ * an UPDATE statement) is an empty resultset. An empty resultset
+ * is complete since the very beginning, and fields() will be empty.
+ *
+ * Resultsets contain additional data about the execution of the query,
+ * like the warning count or the last insert ID. You may access this information
+ * **only after the resultset is complete**. Failing to do so results
+ * in undefined behavior.
+ *
+ * Resultsets are default-constructible. A default-constructed resultset has
+ * valid() == false. It is undefined to call any member function on an invalid
+ * resultset, other than assignment. Resultsets are movable but not copyable.
+ */
+template <
+	typename StreamType
+>
 class resultset
 {
 	using channel_type = detail::channel<StreamType>;
@@ -23,33 +57,73 @@ class resultset
 	detail::ok_packet ok_packet_;
 	bool eof_received_ {false};
 public:
+	/// Default constructor.
 	resultset(): channel_(nullptr) {};
+
+	// Private, do not use
 	resultset(channel_type& channel, detail::resultset_metadata&& meta):
 		channel_(&channel), meta_(std::move(meta)) {};
 	resultset(channel_type& channel, detail::bytestring&& buffer, const detail::ok_packet& ok_pack):
 		channel_(&channel), buffer_(std::move(buffer)), ok_packet_(ok_pack), eof_received_(true) {};
 
+	/**
+	 * \brief Fetches a single row (sync with error code version).
+	 * \details The returned object will be nullptr if there are no more rows
+	 * to be read. Calling fetch_one on a complete resultset returns nullptr.
+	 *
+	 * The returned row points into memory owned by the resultset. Destroying
+	 * or moving the resultset object invalidates the returned row. Calling
+	 * any of the fetch methods again does also invalidate the returned row.
+	 * fetch_one is the fetch method that performs the less memory allocations
+	 * of the three.
+	 */
 	const row* fetch_one(error_code& err);
+
+	/// Fetches a single row (sync with exceptions version).
 	const row* fetch_one();
+
+	/**
+	 * \brief Fetches at most count rows (sync with error code version).
+	 * \details The returned rows are guaranteed to be valid as long as the
+	 * resultset is alive. Contrary to fetch_one, subsequent calls to any of
+	 * the fetch methods do not invalidate the returned rows.
+	 *
+	 * Only if count is **greater** than the available number of rows,
+	 * the resultset will be completed.
+	 */
 	std::vector<owning_row> fetch_many(std::size_t count, error_code& err);
+
+	/// Fetches at most count rows (sync with exceptions version).
 	std::vector<owning_row> fetch_many(std::size_t count);
+
+	/**
+	 * \brief Fetches all available rows (sync with error code version).
+	 * \details The returned rows are guaranteed to be valid as long as the
+	 * resultset is alive. Contrary to fetch_one, subsequent calls to any of
+	 * the fetch methods do not invalidate the returned rows.
+	 *
+	 * The resultset is guaranteed to be complete() after this call returns.
+	 */
 	std::vector<owning_row> fetch_all(error_code& err);
+
+	/// Fetches all available rows (sync with exceptions version).
 	std::vector<owning_row> fetch_all();
 
+	/// Fetchs a single row (async version).
 	template <typename CompletionToken>
 	BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, void(error_code, const row*))
 	async_fetch_one(CompletionToken&& token);
 
+	/// Fetches at most count rows (async version).
 	template <typename CompletionToken>
 	BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, void(error_code, std::vector<owning_row>))
 	async_fetch_many(std::size_t count, CompletionToken&& token);
 
+	/// Fetches all available rows (async version).
 	template <typename CompletionToken>
 	BOOST_ASIO_INITFN_RESULT_TYPE(CompletionToken, void(error_code, std::vector<owning_row>))
 	async_fetch_all(CompletionToken&& token);
 
-	// Is the read of the resultset complete? Pre-condition to any of the functions
-	// accessing the ok_packet
 	bool valid() const noexcept { return channel_ != nullptr; }
 	bool complete() const noexcept { return eof_received_; }
 	const std::vector<field_metadata>& fields() const noexcept { return meta_.fields(); }
