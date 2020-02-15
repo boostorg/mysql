4c63597fbb278a3a30763f048a03b5c858a00989
ruben <rubenperez038@gmail.com>
2020-02-15 02:44:19 +0000

    Removed old integ test functions

--- test/integration/integration_test_common.hpp
+++ test/integration/integration_test_common.hpp
@@ -22,8 +22,6 @@ struct IntegTest : testing::Test
 	mysql::connection_params connection_params {"integ_user", "integ_password", "awesome"};
 	boost::asio::io_context ctx;
 	mysql::connection<boost::asio::ip::tcp::socket> conn {ctx};
-	mysql::error_code errc;
-	mysql::error_info info;
 	boost::asio::executor_work_guard<boost::asio::io_context::executor_type> guard { ctx.get_executor() };
 	std::thread runner {[this]{ ctx.run(); } };
 
@@ -33,7 +31,6 @@ struct IntegTest : testing::Test
 		{
 			boost::asio::ip::tcp::endpoint endpoint (boost::asio::ip::address_v4::loopback(), 3306);
 			conn.next_level().connect(endpoint);
-			reset_errors();
 		}
 		catch (...) // prevent terminate without an active exception on connect error
 		{
@@ -45,85 +42,10 @@ struct IntegTest : testing::Test
 
 	~IntegTest()
 	{
+		error_code errc;
 		conn.next_level().close(errc);
 		guard.reset();
 		runner.join();
-	}
-
-	template <typename Callable>
-	void validate_async_fail(
-		Callable&& initiator,
-		error_code expected_errc,
-		const std::vector<std::string>& expected_info
-	)
-	{
-		std::promise<std::pair<error_code, error_info>> prom;
-		initiator([&prom](error_code errc, error_info info, auto&&...) {
-			prom.set_value(std::make_pair(errc, std::move(info)));
-		});
-		auto [actual_errc, actual_info] = prom.get_future().get();
-		EXPECT_EQ(actual_errc, expected_errc);
-		validate_error_info(actual_info, expected_info);
-	}
-
-	template <typename Callable>
-	void validate_async_fail(
-		Callable&& initiator,
-		Error expected_errc,
-		const std::vector<std::string>& expected_info
-	)
-	{
-		validate_async_fail(
-			std::forward<Callable>(initiator),
-			::mysql::detail::make_error_code(expected_errc),
-			expected_info
-		);
-	}
-
-	template <typename Callable>
-	void validate_sync_fail(
-		Callable&& cb,
-		error_code expected_errc,
-		const std::vector<std::string>& expected_msg
-	)
-	{
-		try
-		{
-			std::forward<Callable>(cb)();
-			FAIL() << "Expected error: " << expected_errc.message();
-		}
-		catch (const boost::system::system_error& err)
-		{
-			EXPECT_EQ(err.code(), expected_errc);
-			validate_string_contains(err.what(), expected_msg);
-		}
-	}
-
-	template <typename Callable>
-	void validate_sync_fail(
-		Callable&& cb,
-		Error expected_errc,
-		const std::vector<std::string>& expected_msg
-	)
-	{
-		validate_sync_fail(std::forward<Callable>(cb), detail::make_error_code(expected_errc), expected_msg);
-	}
-
-	void validate_sync_fail(
-		error_code expected_errc,
-		const std::vector<std::string>& expected_msg
-	)
-	{
-		EXPECT_EQ(errc, expected_errc);
-		validate_string_contains(info.message(), expected_msg);
-	}
-
-	void validate_sync_fail(
-		Error expected_errc,
-		const std::vector<std::string>& expected_msg
-	)
-	{
-		validate_sync_fail(detail::make_error_code(expected_errc), expected_msg);
 	}
 
 	void handshake()
@@ -131,23 +53,8 @@ struct IntegTest : testing::Test
 		conn.handshake(connection_params);
 	}
 
-	// To ensure we clear things passed by lvalue reference
-	void reset_errors()
-	{
-		errc = detail::make_error_code(mysql::Error::no);
-		info.set_message("Previous error message was not cleared correctly");
-	}
-
-	void validate_no_error()
-	{
-		ASSERT_EQ(errc, error_code());
-		EXPECT_EQ(info, error_info());
-	}
-
-	using resultset_type = mysql::resultset<boost::asio::ip::tcp::socket>;
-
 	void validate_eof(
-		const resultset_type& result,
+		const tcp_resultset& result,
 		int affected_rows=0,
 		int warnings=0,
 		int last_insert=0,
@@ -174,7 +81,7 @@ struct IntegTest : testing::Test
 	}
 
 	void validate_2fields_meta(
-		const resultset_type& result,
+		const tcp_resultset& result,
 		const std::string& table
 	) const
 	{
