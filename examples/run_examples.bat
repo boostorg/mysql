701808eed11d63c94ea6683998d3f88e5151068b
ruben <rubenperez038@gmail.com>
2020-03-23 17:08:15 +0000

    Now run examples scripts are autogenerated

--- examples/run_examples.bat
+++
@@ -1,7 +0,0 @@
-SET SCRIPTPATH=%~dp0
-
-mysql.exe -u root < "%SCRIPTPATH%db_setup.sql"
-example_query_sync            example_user example_password
-example_query_async_callbacks example_user example_password
-example_metadata              example_user example_password
-example_prepared_statements   example_user example_password
