5e8303749e2d3932a9437bf1448440d095e17984
ruben <rubenperez038@gmail.com>
2020-03-28 19:47:35 +0000

    Improved docs

--- examples/query_async_coroutines.cpp
+++ examples/query_async_coroutines.cpp
@@ -29,9 +29,10 @@ void print_employee(const boost::mysql::row& employee)
 			  << employee.values()[2] << " dollars yearly\n";  // salary     (type double)
 }
 
+// Throws an exception if an operation failed
 void check_error(
 	const error_code& err,
-	const boost::mysql::error_info& info = boost::mysql::error_info()
+	const error_info& info = {}
 )
 {
 	if (err)
@@ -63,35 +64,68 @@ void main_impl(int argc, char** argv)
 		"mysql_asio_examples"  // database to use; leave empty or omit the parameter for no database
 	);
 
-	// Entry point
+	/**
+	 * The entry point. We spawn a stackful coroutine using boost::asio::spawn
+	 * (see https://www.boost.org/doc/libs/1_72_0/doc/html/boost_asio/reference/spawn.html).
+	 *
+	 * The coroutine will actually start running when we call io_context::run().
+	 * It will suspend every time we call one of the asyncrhonous functions, saving
+	 * all information it needs for resuming. When the asynchronous operation completes,
+	 * the coroutine will resume in the point it was left.
+	 *
+	 * The return type of a coroutine is the second argument to the handler signature
+	 * for the asynchronous operation. For example, connection::connect has a handler
+	 * signature of void(error_code, error_info), so the coroutine return type is error_info.
+	 *
+	 * Coroutines are limited to returning a single argument, so all handler signatures
+	 * in boost::mysql are limited to two arguments. In boost::mysql, coroutines may return:
+	 *   - error_info. Provides additional information in case of error.
+	 *   - async_handler_arg<T>. A combination of a value of type T and an error_info.
+	 *     Used by functions like connection::async_query(), which has to transmit
+	 *     a resultset as a return value, in addition to the error_info.
+	 */
 	boost::asio::spawn(ctx.get_executor(), [&conn, ep, params](boost::asio::yield_context yield) {
+		// This error_code will be filled if an operation fails. We will check it
+		// for every operation we perform.
 		boost::mysql::error_code ec;
 
 		// TCP connect
 		conn.next_layer().async_connect(ep, yield[ec]);
 		check_error(ec);
 
-		// MySQL handshake
+		// MySQL handshake. Note that if the operation would fail,
+		// the returned error_info would contain additional information about what happened
 		boost::mysql::error_info errinfo = conn.async_handshake(params, yield[ec]);
 		check_error(ec, errinfo);
 
-		// Issue the query to the server
+		// Issue the query to the server. This returns an async_handler_arg<tcp_resultset>,
+		// which contains an error_info and a tcp_resultset. Call async_handler_arg::error()
+		// to obtain the error_info, which will contain additional info in case of error.
+		// async_handler_arg::get() returns the actual resultset.
 		const char* sql = "SELECT first_name, last_name, salary FROM employee WHERE company_id = 'HGS'";
 		boost::mysql::async_handler_arg<boost::mysql::tcp_resultset> result =
 				conn.async_query(sql, yield[ec]);
-		check_error(ec, result.error());
+		check_error(ec, result.error()); // The error_info
 
-		// Get all the rows in the resultset
+		/**
+		 * Get all rows in the resultset. We will employ resultset::async_fetch_one(),
+		 * which returns a single row at every call. The returned row is a pointer
+		 * to memory owned by the resultset, and is re-used for each row. Thus, returned
+		 * rows remain valid until the next call to async_fetch_one(). When no more
+		 * rows are available, async_fetch_one returns nullptr.
+		 */
 		while (true)
 		{
 			boost::mysql::async_handler_arg<const boost::mysql::row*> row =
 					result.get().async_fetch_one(yield[ec]);
 			check_error(ec, row.error());
-			if (!row.get()) break;
+			if (!row.get()) break; // No more rows available
 			print_employee(*row.get());
 		}
 	});
 
+	// Don't forget to call run()! Otherwise, your program
+	// will not spawn the coroutine and will do nothing.
 	ctx.run();
 }
 
