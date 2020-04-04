ac166e6683c36b7c1fcdf5513c5e09e3e9cf5252
ruben <rubenperez038@gmail.com>
2020-04-04 19:28:53 +0100

    Added SSL setup for OSX CIs

--- ci/build_unix.sh
+++ ci/build_unix.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# SSL stuff
-sudo cp ci/unix-ssl.cnf /etc/mysql/conf.d/ssl.cnf
-cp ci/*.pem /tmp
-sudo service mysql restart
+
+cp ci/*.pem /tmp # Copy SSL certs/keys to a known location
+if [ $TRAVIS_OS_NAME == "osx" ]; then
+	brew update
+	brew install $DATABASE
+	sudo cp ci/unix-ssl.cnf /etc/my.cnf
+	sudo mysql.server start
+	if [ $DATABASE == "mariadb" ]; then
+		sudo mysql -u root < ci/root_user_setup.sql
+	fi
+else
+	sudo cp ci/unix-ssl.cnf /etc/mysql/conf.d/ssl.cnf
+	sudo service mysql restart
+fi
+
 
 mkdir -p build
 cd build
