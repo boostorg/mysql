c81f3e9112e2ad62aea274533400b77265611fb5
ruben <rubenperez038@gmail.com>
2020-04-04 17:46:30 +0100

    Added SSL setup for UNIX CIs

--- ci/build_unix.sh
+++ ci/build_unix.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# SSL stuff
+sudo cp ci/unix-ssl.cnf /etc/mysql/conf.d/ssl.cnf
+cp ci/*.pem /tmp
+sudo service mysql restart
+
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE $CMAKE_OPTIONS .. 
