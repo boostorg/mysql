fad1a22ca2dba94e24c2587ee94d683c6dc5892b
ruben <rubenperez038@gmail.com>
2020-04-05 10:54:14 +0100

    Corrected unix-ssl.cnf

--- ci/build_unix.sh
+++ ci/build_unix.sh
@@ -5,10 +5,8 @@ cp ci/*.pem /tmp # Copy SSL certs/keys to a known location
 if [ $TRAVIS_OS_NAME == "osx" ]; then
 	brew update
 	brew install $DATABASE
-	sudo bash -c "printf \"[mysqld]\n!include $(pwd)/ci/unix-ssl.cnf\n\" >> /etc/my.cnf"
-	sudo cat /etc/my.cnf
-	sudo mysql.server start || echo "Failed to start server!!"
-	cat /usr/local/var/mysql/Traviss-Mac.local.err
+	sudo cp ci/unix-ssl.cnf /etc/my.cnf
+	sudo mysql.server start
 	exit 1
 	if [ $DATABASE == "mariadb" ]; then
 		sudo mysql -u root < ci/root_user_setup.sql
