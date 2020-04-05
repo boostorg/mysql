1f8b3e8f725e8c6272617aae7479244a3611890e
ruben <rubenperez038@gmail.com>
2020-04-05 11:18:15 +0100

    Debugging Travis OSX

--- ci/build_unix.sh
+++ ci/build_unix.sh
@@ -5,9 +5,14 @@ cp ci/*.pem /tmp # Copy SSL certs/keys to a known location
 if [ $TRAVIS_OS_NAME == "osx" ]; then
 	brew update
 	brew install $DATABASE
-	sudo cp ci/unix-ssl.cnf /etc/my.cnf
+	echo "Base path to database: $(brew --prefix $DATABASE)"
+	find $(brew --prefix $DATABASE)
+	find $(brew --prefix $DATABASE) -name "*.cnf" | xargs -I {} bash -c "echo '----Printing file {}' && cat {}"
+	
+	#sudo bash -c "printf \"[mysqld]\n!include $(pwd)/ci/unix-ssl.cnf\n\" >> /etc/my.cnf"
+	#sudo cat /etc/my.cnf
 	sudo mysql.server start
-	exit 1
+	
 	if [ $DATABASE == "mariadb" ]; then
 		sudo mysql -u root < ci/root_user_setup.sql
 	fi
