77e98230a3fcb14a8f3756033bc139e1c1df1081
Ruben Perez <rubenperez038@gmail.com>
2022-07-26 13:30:03 +0200

    rows_view

--- include/boost/mysql/row.hpp
+++ include/boost/mysql/row.hpp
@@ -131,7 +131,7 @@ public:
     value operator[](std::size_t i) const noexcept { return values_[i]; }
     value front() const noexcept { return values_[0]; }
     value back() const noexcept { return values_[size_ - 1]; }
-    bool empty() const noexcept { return size_ != 0; }
+    bool empty() const noexcept { return size_ == 0; }
     std::size_t size() const noexcept { return size_; }
 };
 
@@ -186,6 +186,62 @@ public:
         // TODO: this can be made random access
 
         iterator(const rows* obj, std::size_t rownum) noexcept : obj_(obj), row_num_(rownum) {}
+
+        iterator& operator++() noexcept { ++row_num_; return *this; }
+        iterator operator++(int) noexcept { return iterator(obj_, row_num_ + 1); }
+        iterator& operator--() noexcept { --row_num_; return *this; }
+        iterator operator--(int) noexcept { return iterator(obj_, row_num_ - 1); }
+        reference operator*() const noexcept { return (*obj_)[row_num_]; }
+        bool operator==(const iterator& rhs) const noexcept { return obj_ == rhs.obj_&& row_num_ == rhs.row_num_; }
+        bool operator!=(const iterator& rhs) const noexcept { return !(*this == rhs); }
+    };
+};
+
+
+class rows_view
+{
+    const value* values_ {};
+    std::size_t num_values_ {};
+    std::size_t num_columns_ {};
+public:
+    rows_view() = default;
+    rows_view(const value* values, std::size_t num_values, std::size_t num_columns) noexcept :
+        values_(values),
+        num_values_(num_values),
+        num_columns_(num_columns)
+    {
+    }
+
+    class iterator;
+    using const_iterator = iterator;
+    // TODO: add other standard container members
+
+    iterator begin() const noexcept { return iterator(this, 0); }
+    iterator end() const noexcept { return iterator(this, size()); }
+    row_view at(std::size_t i) const { /* TODO: check idx */ return (*this)[i]; }
+    row_view operator[](std::size_t i) const noexcept
+    {
+        std::size_t offset = num_columns_ * i;
+        return row_view(values_ + offset, num_columns_);
+    }
+    row_view front() const noexcept { return (*this)[0]; }
+    row_view back() const noexcept { return (*this)[size() - 1]; }
+    bool empty() const noexcept { return num_values_ == 0; }
+    std::size_t size() const noexcept { return num_values_ / num_columns_; }
+
+    class iterator
+    {
+        const rows_view* obj_;
+        std::size_t row_num_;
+    public:
+        using value_type = row_view; // TODO: should this be row?
+        using reference = row_view;
+        using pointer = void const*;
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::bidirectional_iterator_tag;
+        // TODO: this can be made random access
+
+        iterator(const rows_view* obj, std::size_t rownum) noexcept : obj_(obj), row_num_(rownum) {}
 
         iterator& operator++() noexcept { ++row_num_; return *this; }
         iterator operator++(int) noexcept { return iterator(obj_, row_num_ + 1); }
