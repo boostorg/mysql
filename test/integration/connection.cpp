1b5d4a6a1ceaa47cc4c335551b997e8768d9840c
Ruben Perez <rubenperez038@gmail.com>
2022-10-15 12:50:58 +0200

    connection integ tests

--- test/integration/connection.cpp
+++ test/integration/connection.cpp
@@ -6,47 +6,61 @@
 //
 
 #include <boost/mysql/connection.hpp>
-#include "integration_test_common.hpp"
+#include <boost/mysql/tcp.hpp>
+
 #include "get_endpoint.hpp"
+#include "integration_test_common.hpp"
 
 using namespace boost::mysql::test;
-using boost::mysql::connection;
 using boost::asio::ip::tcp;
+using boost::mysql::tcp_connection;
+using boost::mysql::tcp_resultset;
+
+namespace {
 
 BOOST_AUTO_TEST_SUITE(test_connection)
 
 BOOST_FIXTURE_TEST_CASE(move_constructor_connected_connection, network_fixture)
 {
     // First connection
-    connection<tcp::socket> first (ctx);
+    tcp_connection first(ctx);
     BOOST_TEST(first.valid());
 
-    // Connect
+    // Connect and use
+    tcp_resultset result;
     first.connect(get_endpoint<tcp::socket>(er_endpoint::valid), params);
+    first.query("SELECT 1", result);
+    result.read_all();
 
     // Construct second connection
-    connection<tcp::socket> second (std::move(first));
-    BOOST_TEST(!first.valid());
-    BOOST_TEST(second.valid());
-    second.query("SELECT 1"); // doesn't throw
+    tcp_connection second(std::move(first));
+    BOOST_TEST_REQUIRE(second.valid());
+    second.query("SELECT 1", result);
+    BOOST_TEST(result.read_all().at(0).at(0).as_int64() == 1);
 }
 
 BOOST_FIXTURE_TEST_CASE(move_assignment_from_connected_connection, network_fixture)
 {
-    // First connection
-    connection<tcp::socket> first (ctx);
-    connection<tcp::socket> second (ctx);
-    BOOST_TEST(first.valid());
-    BOOST_TEST(second.valid());
+    // Connections
+    tcp_connection first(ctx);
+    tcp_connection second(ctx);
 
-    // Connect
+    // Connect and use both
+    tcp_resultset result;
     first.connect(get_endpoint<tcp::socket>(er_endpoint::valid), params);
+    second.connect(get_endpoint<tcp::socket>(er_endpoint::valid), params);
+    first.query("SELECT 1", result);
+    result.read_all();
+    second.query("SELECT 2", result);
+    result.read_all();
 
     // Move
     second = std::move(first);
-    BOOST_TEST(!first.valid());
     BOOST_TEST(second.valid());
-    second.query("SELECT 1"); // doesn't throw
+    second.query("SELECT 4", result);
+    BOOST_TEST(result.read_all().at(0).at(0).as_int64() == 4);
 }
 
-BOOST_AUTO_TEST_SUITE_END() // test_connection
+BOOST_AUTO_TEST_SUITE_END()  // test_connection
+
+}  // namespace
