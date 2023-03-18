0dd1f1f87c9d1d3d7bf185187f1458acd13b30f6
Ruben Perez <rubenperez038@gmail.com>
2023-03-18 21:24:24 +0100

    start_execution tests

--- test/unit/detail/network_algorithms/start_execution.cpp
+++ test/unit/detail/network_algorithms/start_execution.cpp
@@ -10,6 +10,7 @@
 #include <boost/mysql/column_type.hpp>
 #include <boost/mysql/diagnostics.hpp>
 #include <boost/mysql/error_code.hpp>
+#include <boost/mysql/field_view.hpp>
 
 #include <boost/mysql/detail/network_algorithms/start_execution.hpp>
 #include <boost/mysql/detail/protocol/constants.hpp>
@@ -31,19 +32,15 @@ using boost::mysql::blob;
 using boost::mysql::client_errc;
 using boost::mysql::column_type;
 using boost::mysql::error_code;
-using boost::mysql::detail::execution_state_impl;
+using boost::mysql::execution_state;
+using boost::mysql::field_view;
 using boost::mysql::detail::protocol_field_type;
 using boost::mysql::detail::resultset_encoding;
 using namespace boost::mysql::test;
 
 namespace {
 
-using netfun_maker = netfun_maker_fn<
-    void,
-    test_channel&,
-    error_code,
-    resultset_encoding,
-    execution_state_impl&>;
+using netfun_maker = netfun_maker_fn<void, test_channel&, resultset_encoding, execution_state&>;
 
 struct
 {
@@ -54,15 +51,6 @@ struct
     {netfun_maker::async_errinfo(&boost::mysql::detail::async_start_execution), "async"}
 };
 
-// Verify that we reset the state
-execution_state_impl create_initial_state()
-{
-    return exec_builder(false, resultset_encoding::text)
-        .meta({protocol_field_type::geometry})
-        .seqnum(4)
-        .build();
-}
-
 BOOST_AUTO_TEST_SUITE(test_start_execution)
 
 BOOST_AUTO_TEST_CASE(success)
@@ -71,18 +59,26 @@ BOOST_AUTO_TEST_CASE(success)
     {
         BOOST_TEST_CONTEXT(fns.name)
         {
+            // Initial state, to verify that we reset it
+            std::vector<field_view> fields;
+            auto st = exec_builder(false)
+                          .reset(resultset_encoding::text, &fields)
+                          .meta({protocol_field_type::geometry})
+                          .seqnum(4)
+                          .build_state();
+
+            // Channel
             auto chan = create_channel();
             chan.lowest_layer()
                 .add_message(create_message(1, {0x01}))
                 .add_message(create_coldef_message(2, protocol_field_type::var_string));
             chan.shared_sequence_number() = 42u;
-            auto st = create_initial_state();
 
             // Get an execution request into the channel's buffer
             concat(chan.shared_buffer(), {0x02, 0x05, 0x09});
 
             // Call the function
-            fns.start_execution(chan, error_code(), resultset_encoding::binary, st).validate_no_error();
+            fns.start_execution(chan, resultset_encoding::binary, st).validate_no_error();
 
             // We've written the request message
             auto expected_msg = create_message(0, {0x02, 0x05, 0x09});
@@ -90,32 +86,13 @@ BOOST_AUTO_TEST_CASE(success)
             BOOST_TEST(chan.shared_sequence_number() == 42u);  // unused
 
             // We've read the response
-            BOOST_TEST(st.encoding() == resultset_encoding::binary);
-            BOOST_TEST(st.sequence_number() == 3u);
+            BOOST_TEST(get_impl(st).encoding() == resultset_encoding::binary);
+            BOOST_TEST(get_impl(st).sequence_number() == 3u);
             BOOST_TEST(st.should_read_rows());
-            check_meta(st.current_resultset_meta(), {std::make_pair(column_type::varchar, "mycol")});
-        }
-    }
-}
-
-BOOST_AUTO_TEST_CASE(error_fast_fail)
-{
-    for (auto fns : all_fns)
-    {
-        BOOST_TEST_CONTEXT(fns.name)
-        {
-            auto chan = create_channel(ok_msg_builder().build_ok());
-            auto st = create_initial_state();
-
-            // Get an execution request into the channel's buffer
-            concat(chan.shared_buffer(), {0x02, 0x05, 0x09});
-
-            // Call the function
-            fns.start_execution(chan, client_errc::wrong_num_params, resultset_encoding::binary, st)
-                .validate_error_exact(client_errc::wrong_num_params);
-
-            // We didn't write the message
-            BOOST_MYSQL_ASSERT_BLOB_EQUALS(chan.lowest_layer().bytes_written(), blob());
+            check_meta(
+                get_impl(st).current_resultset_meta(),
+                {std::make_pair(column_type::varchar, "mycol")}
+            );
         }
     }
 }
@@ -131,18 +108,19 @@ BOOST_AUTO_TEST_CASE(error_network_error)
             {
                 BOOST_TEST_CONTEXT(i)
                 {
+                    std::vector<field_view> fields;
+                    auto st = exec_builder(false).reset(&fields).build_state();
                     auto chan = create_channel();
                     chan.lowest_layer()
                         .add_message(create_message(1, {0x01}))
                         .add_message(create_coldef_message(2, protocol_field_type::var_string))
                         .set_fail_count(fail_count(i, client_errc::server_unsupported));
-                    auto st = create_initial_state();
 
                     // Get an execution request into the channel's buffer
                     concat(chan.shared_buffer(), {0x02, 0x05, 0x09});
 
                     // Call the function
-                    fns.start_execution(chan, error_code(), resultset_encoding::binary, st)
+                    fns.start_execution(chan, resultset_encoding::binary, st)
                         .validate_error_exact(client_errc::server_unsupported);
                 }
             }
