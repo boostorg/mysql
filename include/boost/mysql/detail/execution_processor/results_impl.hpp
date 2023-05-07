c6fc8b3440d9e7a2ab207cd5d7196658145bbcf7
Ruben Perez <rubenperez038@gmail.com>
2023-05-07 14:23:41 +0200

    Merged results_impl and multi_rows

--- include/boost/mysql/detail/execution_processor/results_impl.hpp
+++ include/boost/mysql/detail/execution_processor/results_impl.hpp
@@ -25,78 +25,6 @@ namespace boost {
 namespace mysql {
 namespace detail {
 
-// A rows-like collection that can hold the rows for multiple resultsets.
-// This class also helps gathering all the rows without incurring in dangling references.
-// Rows are read in batches:
-// - Open a batch calling start_batch()
-// - Call deserialize_row(), which will call add_row(). This adds raw storage for fields.
-//   Fields are then deserialized there. string/blob field_view's point into the
-//   connection's internal buffer.
-// - When the batch finishes, call finish_batch(). This will copy strings into thr row_impl
-//   and transform these values into offsets, so the buffer can be grown.
-// - When the final OK packet is received, offsets are converted back into views,
-//   by calling finish()
-
-class multi_rows
-{
-    static constexpr std::size_t no_batch = std::size_t(-1);
-
-    row_impl impl_;
-    std::size_t num_fields_at_batch_start_{no_batch};
-
-    bool has_active_batch() const noexcept { return num_fields_at_batch_start_ != no_batch; }
-
-public:
-    multi_rows() = default;
-
-    std::size_t num_fields() const noexcept { return impl_.fields().size(); }
-
-    rows_view rows_slice(std::size_t offset, std::size_t num_columns, std::size_t num_rows) const noexcept
-    {
-        return rows_view_access::construct(
-            impl_.fields().data() + offset,
-            num_rows * num_columns,
-            num_columns
-        );
-    }
-
-    void reset() noexcept
-    {
-        impl_.clear();
-        num_fields_at_batch_start_ = no_batch;
-    }
-
-    void start_batch() noexcept
-    {
-        assert(!has_active_batch());
-        num_fields_at_batch_start_ = num_fields();
-    }
-
-    field_view* add_row(std::size_t num_fields)
-    {
-        assert(has_active_batch());
-        return impl_.add_fields(num_fields);
-    }
-
-    void finish_batch()
-    {
-        if (has_active_batch())
-        {
-            impl_.copy_strings_as_offsets(
-                num_fields_at_batch_start_,
-                impl_.fields().size() - num_fields_at_batch_start_
-            );
-            num_fields_at_batch_start_ = no_batch;
-        }
-    }
-
-    void finish()
-    {
-        finish_batch();
-        impl_.offsets_to_string_views();
-    }
-};
-
 struct per_resultset_data
 {
     std::size_t num_columns{};       // Number of columns this resultset has
@@ -162,6 +90,13 @@ public:
     }
 };
 
+// Rows for all resultsets are stored in a single rows_impl object.
+// - When a row batch is started, we record how many fields we had before the batch.
+// - When rows are read, fields are allocated in the rows_impl object, then deserialized against
+//   the allocated storage. At this point, strings/blobs point into the connection read buffer.
+// - When a row batch is finished, we copy strings/blobs into the rows_impl, then transform them
+//   into offsets to allow rows_impl to grow.
+// - When the final OK packet is received, offsets are transformed back into views.
 class results_impl final : public execution_processor
 {
 public:
@@ -186,8 +121,11 @@ public:
     rows_view get_rows(std::size_t index) const noexcept
     {
         const auto& resultset_data = per_result_[index];
-        return rows_
-            .rows_slice(resultset_data.field_offset, resultset_data.num_columns, resultset_data.num_rows);
+        return rows_view_access::construct(
+            rows_.fields().data() + resultset_data.field_offset,
+            resultset_data.num_rows * resultset_data.num_columns,
+            resultset_data.num_columns
+        );
     }
     metadata_collection_view get_meta(std::size_t index) const noexcept
     {
@@ -234,6 +172,8 @@ private:
         meta_.clear();
         per_result_.clear();
         info_.clear();
+        rows_.clear();
+        num_fields_at_batch_start_ = no_batch;
     }
 
     void on_num_meta_impl(std::size_t num_columns) override
@@ -270,9 +210,11 @@ private:
 
     error_code on_row_impl(deserialization_context ctx, const output_ref&) override
     {
+        assert(has_active_batch());
+
         // add row storage
         std::size_t num_fields = current_resultset().num_columns;
-        field_view* storage = rows_.add_row(num_fields);
+        field_view* storage = rows_.add_fields(num_fields);
         ++current_resultset().num_rows;
 
         // deserialize the row
@@ -283,18 +225,39 @@ private:
         return error_code();
     }
 
-    void on_row_batch_start_impl() override final { rows_.start_batch(); }
+    void on_row_batch_start_impl() override final
+    {
+        assert(!has_active_batch());
+        num_fields_at_batch_start_ = rows_.fields().size();
+    }
 
-    void on_row_batch_finish_impl() override final { rows_.finish_batch(); }
+    void on_row_batch_finish_impl() override final { finish_batch(); }
 
     // Data
     std::size_t remaining_meta_{};
     std::vector<metadata> meta_;
     resultset_container per_result_;
     std::vector<char> info_;
-    multi_rows rows_;
+    row_impl rows_;
+    std::size_t num_fields_at_batch_start_{no_batch};
 
     // Auxiliar
+    static constexpr std::size_t no_batch = std::size_t(-1);
+
+    bool has_active_batch() const noexcept { return num_fields_at_batch_start_ != no_batch; }
+
+    void finish_batch()
+    {
+        if (has_active_batch())
+        {
+            rows_.copy_strings_as_offsets(
+                num_fields_at_batch_start_,
+                rows_.fields().size() - num_fields_at_batch_start_
+            );
+            num_fields_at_batch_start_ = no_batch;
+        }
+    }
+
     per_resultset_data& current_resultset() noexcept
     {
         assert(!per_result_.empty());
@@ -312,7 +275,7 @@ private:
         // Allocate a new per-resultset object
         auto& resultset_data = per_result_.emplace_back();
         resultset_data.meta_offset = meta_.size();
-        resultset_data.field_offset = rows_.num_fields();
+        resultset_data.field_offset = rows_.fields().size();
         resultset_data.info_offset = info_.size();
         return resultset_data;
     }
@@ -333,7 +296,8 @@ private:
         }
         else
         {
-            rows_.finish();
+            finish_batch();
+            rows_.offsets_to_string_views();
             set_state(state_t::complete);
         }
     }
