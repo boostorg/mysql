eaa953812aa42b447ecde9aa8137220ebcd877a2
Ruben Perez <rubenperez038@gmail.com>
2023-05-11 12:56:08 +0200

    Fixed MariaDB-specific type error issue

--- test/common/creation/create_meta.hpp
+++ test/common/creation/create_meta.hpp
@@ -21,31 +21,80 @@ namespace boost {
 namespace mysql {
 namespace test {
 
-class meta_builder
+class coldef_builder
 {
     detail::column_definition_packet pack_{};
+
+public:
+    coldef_builder() = default;
+    coldef_builder& type(detail::protocol_field_type v) noexcept
+    {
+        pack_.type = v;
+        return *this;
+    }
+    coldef_builder& flags(std::uint16_t v) noexcept
+    {
+        pack_.flags = v;
+        return *this;
+    }
+    coldef_builder& decimals(std::uint8_t v) noexcept
+    {
+        pack_.decimals = v;
+        return *this;
+    }
+    coldef_builder& collation(std::uint16_t v) noexcept
+    {
+        pack_.character_set = v;
+        return *this;
+    }
+    coldef_builder& unsigned_flag(bool v) noexcept
+    {
+        if (v)
+            pack_.flags |= detail::column_flags::unsigned_;
+        else
+            pack_.flags &= ~detail::column_flags::unsigned_;
+        return *this;
+    }
+    coldef_builder& nullable(bool v) noexcept
+    {
+        if (v)
+            pack_.flags &= ~detail::column_flags::not_null;
+        else
+            pack_.flags |= detail::column_flags::not_null;
+        return *this;
+    }
+    coldef_builder& name(string_view v) noexcept
+    {
+        pack_.name.value = v;
+        return *this;
+    }
+    detail::column_definition_packet build() { return pack_; }
+};
+
+class meta_builder
+{
+    coldef_builder b_;
     bool coltype_set_{false};
     column_type type_{};
 
 public:
     meta_builder(detail::protocol_field_type t = detail::protocol_field_type::enum_)
     {
-        pack_.type = t;
-        pack_.character_set = 33;  // utf8_general_ci
+        b_.type(t).collation(33);  // utf8_general_ci
     }
     meta_builder& flags(std::uint16_t v) noexcept
     {
-        pack_.flags = v;
+        b_.flags(v);
         return *this;
     }
     meta_builder& decimals(std::uint8_t v) noexcept
     {
-        pack_.decimals = v;
+        b_.decimals(v);
         return *this;
     }
     meta_builder& collation(std::uint16_t v) noexcept
     {
-        pack_.character_set = v;
+        b_.collation(v);
         return *this;
     }
     meta_builder& type(column_type v) noexcept
@@ -56,28 +105,22 @@ public:
     }
     meta_builder& unsigned_flag(bool v) noexcept
     {
-        if (v)
-            pack_.flags |= detail::column_flags::unsigned_;
-        else
-            pack_.flags &= ~detail::column_flags::unsigned_;
+        b_.unsigned_flag(v);
         return *this;
     }
     meta_builder& nullable(bool v) noexcept
     {
-        if (v)
-            pack_.flags &= ~detail::column_flags::not_null;
-        else
-            pack_.flags |= detail::column_flags::not_null;
+        b_.nullable(v);
         return *this;
     }
     meta_builder& name(string_view v) noexcept
     {
-        pack_.name.value = v;
+        b_.name(v);
         return *this;
     }
     metadata build()
     {
-        auto res = detail::metadata_access::construct(pack_, true);
+        auto res = detail::metadata_access::construct(b_.build(), true);
         if (coltype_set_)
             detail::metadata_access::set_type(res, type_);
         return res;
