3c42d384fac284ee4e01744025b4cf8666ab0213
Ruben Perez <rubenperez038@gmail.com>
2023-06-05 15:14:06 +0200

    Moved column_type

--- include/boost/mysql/column_type.hpp
+++ include/boost/mysql/column_type.hpp
@@ -8,8 +8,9 @@
 #ifndef BOOST_MYSQL_COLUMN_TYPE_HPP
 #define BOOST_MYSQL_COLUMN_TYPE_HPP
 
-#include <cstdint>
-#include <ostream>
+#include <boost/mysql/detail/config.hpp>
+
+#include <iosfwd>
 
 namespace boost {
 namespace mysql {
@@ -56,37 +57,8 @@ enum class column_type
 /**
  * \brief Streams a `column_type`.
  */
-inline std::ostream& operator<<(std::ostream& os, column_type t)
-{
-    switch (t)
-    {
-    case column_type::tinyint: return os << "tinyint";
-    case column_type::smallint: return os << "smallint";
-    case column_type::mediumint: return os << "mediumint";
-    case column_type::int_: return os << "int_";
-    case column_type::bigint: return os << "bigint";
-    case column_type::float_: return os << "float_";
-    case column_type::double_: return os << "double_";
-    case column_type::decimal: return os << "decimal";
-    case column_type::bit: return os << "bit";
-    case column_type::year: return os << "year";
-    case column_type::time: return os << "time";
-    case column_type::date: return os << "date";
-    case column_type::datetime: return os << "datetime";
-    case column_type::timestamp: return os << "timestamp";
-    case column_type::char_: return os << "char_";
-    case column_type::varchar: return os << "varchar";
-    case column_type::binary: return os << "binary";
-    case column_type::varbinary: return os << "varbinary";
-    case column_type::text: return os << "text";
-    case column_type::blob: return os << "blob";
-    case column_type::enum_: return os << "enum_";
-    case column_type::set: return os << "set";
-    case column_type::json: return os << "json";
-    case column_type::geometry: return os << "geometry";
-    default: return os << "<unknown column type>";
-    }
-}
+BOOST_MYSQL_DECL
+std::ostream& operator<<(std::ostream& os, column_type t);
 
 }  // namespace mysql
 }  // namespace boost
--- src/interface.cpp
+++ src/interface.cpp
@@ -308,3 +308,36 @@ std::ostream& boost::mysql::operator<<(std::ostream& os, const field_view& value
     default: BOOST_ASSERT(false); return os;
     }
 }
+
+// column_type
+std::ostream& boost::mysql::operator<<(std::ostream& os, column_type t)
+{
+    switch (t)
+    {
+    case column_type::tinyint: return os << "tinyint";
+    case column_type::smallint: return os << "smallint";
+    case column_type::mediumint: return os << "mediumint";
+    case column_type::int_: return os << "int_";
+    case column_type::bigint: return os << "bigint";
+    case column_type::float_: return os << "float_";
+    case column_type::double_: return os << "double_";
+    case column_type::decimal: return os << "decimal";
+    case column_type::bit: return os << "bit";
+    case column_type::year: return os << "year";
+    case column_type::time: return os << "time";
+    case column_type::date: return os << "date";
+    case column_type::datetime: return os << "datetime";
+    case column_type::timestamp: return os << "timestamp";
+    case column_type::char_: return os << "char_";
+    case column_type::varchar: return os << "varchar";
+    case column_type::binary: return os << "binary";
+    case column_type::varbinary: return os << "varbinary";
+    case column_type::text: return os << "text";
+    case column_type::blob: return os << "blob";
+    case column_type::enum_: return os << "enum_";
+    case column_type::set: return os << "set";
+    case column_type::json: return os << "json";
+    case column_type::geometry: return os << "geometry";
+    default: return os << "<unknown column type>";
+    }
+}
