134d350d10d7f5d09fc897b59660ede9d99b26d9
Ruben Perez <rubenperez038@gmail.com>
2023-06-12 23:00:40 +0200

    read_some_rows_dynamic

--- test/unit/network_algorithms/read_some_rows_dynamic.cpp
+++ test/unit/network_algorithms/read_some_rows_dynamic.cpp
@@ -5,53 +5,58 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include "network_algorithms/read_some_rows_dynamic.hpp"
+
+#include <boost/mysql/client_errc.hpp>
+
 #include <boost/mysql/detail/execution_processor/execution_state_impl.hpp>
-#include <boost/mysql/detail/network_algorithms/read_some_rows_dynamic.hpp>
 
 #include <boost/test/unit_test.hpp>
 
-#include "creation/create_execution_processor.hpp"
-#include "creation/create_message.hpp"
-#include "creation/create_message_struct.hpp"
-#include "creation/create_meta.hpp"
-#include "creation/create_row_message.hpp"
-#include "test_channel.hpp"
-#include "test_common.hpp"
-#include "unit_netfun_maker.hpp"
+#include "channel/channel.hpp"
+#include "test_unit/create_channel.hpp"
+#include "test_unit/create_execution_processor.hpp"
+#include "test_unit/create_frame.hpp"
+#include "test_unit/create_meta.hpp"
+#include "test_unit/create_ok.hpp"
+#include "test_unit/create_row_message.hpp"
+#include "test_unit/test_stream.hpp"
+#include "test_unit/unit_netfun_maker.hpp"
 
 using namespace boost::mysql::test;
 using namespace boost::mysql;
+using boost::mysql::detail::channel;
 using boost::mysql::detail::execution_state_impl;
-
-namespace {
 
 BOOST_AUTO_TEST_SUITE(test_read_some_rows_dynamic)
 
-using netfun_maker = netfun_maker_fn<rows_view, test_channel&, execution_state_impl&>;
+using netfun_maker = netfun_maker_fn<rows_view, channel&, execution_state_impl&>;
 
 struct
 {
     typename netfun_maker::signature read_some_rows_dynamic;
     const char* name;
 } all_fns[] = {
-    {netfun_maker::sync_errc(&detail::read_some_rows_dynamic),           "sync" },
-    {netfun_maker::async_errinfo(&detail::async_read_some_rows_dynamic), "async"},
+    {netfun_maker::sync_errc(&detail::read_some_rows_dynamic_impl),           "sync" },
+    {netfun_maker::async_errinfo(&detail::async_read_some_rows_dynamic_impl), "async"},
 };
 
 struct fixture
 {
     execution_state_impl st;
-    test_channel chan;
+    channel chan{create_channel()};
 
-    fixture() : chan(create_channel())
+    fixture()
     {
         // Prepare the state, such that it's ready to read rows
-        add_meta(st, {meta_builder().type(column_type::varchar).build()});
+        add_meta(st, {meta_builder().type(column_type::varchar).build_coldef()});
         st.sequence_number() = 42;
 
         // Put something in shared_fields, simulating a previous read
-        chan.shared_fields().push_back(field_view("prev"));  // from previous read
+        chan.shared_fields().push_back(field_view("prev"));
     }
+
+    test_stream& stream() noexcept { return get_stream(chan); }
 };
 
 BOOST_AUTO_TEST_CASE(eof)
@@ -61,9 +66,7 @@ BOOST_AUTO_TEST_CASE(eof)
         BOOST_TEST_CONTEXT(fns.name)
         {
             fixture fix;
-            fix.chan.lowest_layer().add_message(
-                ok_msg_builder().affected_rows(1).info("1st").seqnum(42).build_eof()
-            );
+            fix.stream().add_bytes(ok_builder().affected_rows(1).info("1st").seqnum(42).build_eof_frame());
 
             rows_view rv = fns.read_some_rows_dynamic(fix.chan, fix.st).get();
             BOOST_TEST(rv == makerows(1));
@@ -82,11 +85,11 @@ BOOST_AUTO_TEST_CASE(batch_with_rows)
         BOOST_TEST_CONTEXT(fns.name)
         {
             fixture fix;
-            fix.chan.lowest_layer()
-                .add_message(
-                    concat_copy(create_text_row_message(42, "abc"), create_text_row_message(43, "von"))
-                )
-                .add_message(create_text_row_message(44, "other"));  // only a single read should be issued
+            fix.stream()
+                .add_bytes(create_text_row_message(42, "abc"))
+                .add_bytes(create_text_row_message(43, "von"))
+                .add_break()
+                .add_bytes(create_text_row_message(44, "other"));  // only a single read should be issued
 
             rows_view rv = fns.read_some_rows_dynamic(fix.chan, fix.st).get();
             BOOST_TEST(rv == makerows(1, "abc", "von"));
@@ -103,11 +106,10 @@ BOOST_AUTO_TEST_CASE(batch_with_rows_eof)
         BOOST_TEST_CONTEXT(fns.name)
         {
             fixture fix;
-            fix.chan.lowest_layer().add_message(concat_copy(
-                create_text_row_message(42, "abc"),
-                create_text_row_message(43, "von"),
-                ok_msg_builder().seqnum(44).affected_rows(1).info("1st").build_eof()
-            ));
+            fix.stream()
+                .add_bytes(create_text_row_message(42, "abc"))
+                .add_bytes(create_text_row_message(43, "von"))
+                .add_bytes(ok_builder().seqnum(44).affected_rows(1).info("1st").build_eof_frame());
 
             rows_view rv = fns.read_some_rows_dynamic(fix.chan, fix.st).get();
             BOOST_TEST(rv == makerows(1, "abc", "von"));
@@ -129,7 +131,7 @@ BOOST_AUTO_TEST_CASE(error)
             fixture fix;
 
             // invalid row
-            fix.chan.lowest_layer().add_message(create_message(42, {0x02, 0xff}));
+            fix.stream().add_bytes(create_frame(42, {0x02, 0xff}));
 
             fns.read_some_rows_dynamic(fix.chan, fix.st)
                 .validate_error_exact(client_errc::incomplete_message);
@@ -138,5 +140,3 @@ BOOST_AUTO_TEST_CASE(error)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-
-}  // namespace
