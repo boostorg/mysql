1371727867aded9f85488a316161f17d6a357892
Ruben Perez <rubenperez038@gmail.com>
2023-06-13 15:00:37 +0200

    execution_concepts

--- test/unit-iface/test/detail/execution_concepts.cpp
+++ test/unit-iface/test/detail/execution_concepts.cpp
@@ -5,23 +5,30 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <boost/mysql/detail/config.hpp>
+
+#ifdef BOOST_MYSQL_HAS_CONCEPTS
+
 #include <boost/mysql/statement.hpp>
 #include <boost/mysql/string_view.hpp>
 
-#include <boost/mysql/detail/config.hpp>
 #include <boost/mysql/detail/execution_concepts.hpp>
 
 #include <string>
-#ifdef __cpp_lib_string_view
 #include <string_view>
-#endif
 
 using namespace boost::mysql;
-using detail::is_execution_request;
+using boost::mysql::detail::execution_state_type;
+using boost::mysql::detail::is_execution_request;
+using boost::mysql::detail::results_type;
+
+namespace {
 
 // -----
-// strings
+// is_execution_request
 // -----
+
+// strings
 static_assert(is_execution_request<std::string>::value, "");
 static_assert(is_execution_request<const std::string&>::value, "");
 static_assert(is_execution_request<std::string&&>::value, "");
@@ -31,20 +38,16 @@ static_assert(is_execution_request<string_view>::value, "");
 static_assert(is_execution_request<const string_view&>::value, "");
 static_assert(is_execution_request<string_view&&>::value, "");
 
-#ifdef __cpp_lib_string_view
 static_assert(is_execution_request<std::string_view>::value, "");
 static_assert(is_execution_request<const std::string_view&>::value, "");
 static_assert(is_execution_request<std::string_view&&>::value, "");
-#endif
 
 static_assert(is_execution_request<const char*>::value, "");
 
 static_assert(is_execution_request<const char[14]>::value, "");
 static_assert(is_execution_request<const char (&)[14]>::value, "");
 
-// -----
 // tuple statements
-// -----
 using tup_type = std::tuple<int, const char*, std::nullptr_t>;
 static_assert(is_execution_request<bound_statement_tuple<tup_type>>::value, "");
 static_assert(is_execution_request<const bound_statement_tuple<tup_type>&>::value, "");
@@ -53,24 +56,51 @@ static_assert(is_execution_request<bound_statement_tuple<tup_type>&&>::value, ""
 
 static_assert(is_execution_request<bound_statement_tuple<std::tuple<>>>::value, "");
 
-// tuples not accepted
 static_assert(!is_execution_request<tup_type>::value, "");
 static_assert(!is_execution_request<const tup_type&>::value, "");
 static_assert(!is_execution_request<tup_type&>::value, "");
 static_assert(!is_execution_request<tup_type&&>::value, "");
 
-// -----
-// iterator statements
-// -----
+// iterator range statements
 static_assert(is_execution_request<bound_statement_iterator_range<field_view*>>::value, "");
 static_assert(is_execution_request<const bound_statement_iterator_range<field_view*>&>::value, "");
 static_assert(is_execution_request<bound_statement_iterator_range<field_view*>&>::value, "");
 static_assert(is_execution_request<bound_statement_iterator_range<field_view*>&&>::value, "");
 
-// iterators not accepted
 static_assert(!is_execution_request<field_view*>::value, "");
 
-// Other stuff not accepted
+// Other stuff
 static_assert(!is_execution_request<field_view>::value, "");
 static_assert(!is_execution_request<int>::value, "");
 static_assert(!is_execution_request<std::nullptr_t>::value, "");
+
+using row1 = std::tuple<int, float>;
+using row2 = std::tuple<double>;
+
+// -----
+// execution_state_type
+// -----
+static_assert(!execution_state_type<std::string>, "");
+static_assert(!execution_state_type<int>, "");
+static_assert(!execution_state_type<results>, "");
+static_assert(!execution_state_type<static_results<row1>>, "");
+static_assert(!execution_state_type<static_results<row1, row2>>, "");
+static_assert(execution_state_type<execution_state>, "");
+static_assert(execution_state_type<static_execution_state<row1>>, "");
+static_assert(execution_state_type<static_execution_state<row1, row2>>, "");
+
+// -----
+// results_type
+// -----
+static_assert(!results_type<std::string>, "");
+static_assert(!results_type<int>, "");
+static_assert(!results_type<execution_state>, "");
+static_assert(!results_type<static_execution_state<row1>>, "");
+static_assert(!results_type<static_execution_state<row1, row2>>, "");
+static_assert(results_type<results>, "");
+static_assert(results_type<static_results<row1>>, "");
+static_assert(results_type<static_results<row1, row2>>, "");
+
+}  // namespace
+
+#endif
