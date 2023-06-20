4b4b0fd36a46ba8d6be25324755bebfbf75f6370
Ruben Perez <rubenperez038@gmail.com>
2023-06-20 16:22:03 +0200

    socket_stream tests

--- include/boost/mysql/detail/socket_stream.hpp
+++ include/boost/mysql/detail/socket_stream.hpp
@@ -9,6 +9,7 @@
 #define BOOST_MYSQL_DETAIL_SOCKET_STREAM_HPP
 
 #include <boost/asio/basic_socket.hpp>
+#include <boost/asio/basic_stream_socket.hpp>
 
 #include <type_traits>
 
@@ -21,9 +22,15 @@ struct is_socket : std::false_type
 {
 };
 
-// typename basic_stream_socket::lowest_layer_type is basic_socket, so we just require basic_socket here
+// typename basic_stream_socket::lowest_layer_type is basic_socket, so we accept basic_socket and
+// basic_stream_socket here
 template <class Protocol, class Executor>
 struct is_socket<asio::basic_socket<Protocol, Executor>> : std::true_type
+{
+};
+
+template <class Protocol, class Executor>
+struct is_socket<asio::basic_stream_socket<Protocol, Executor>> : std::true_type
 {
 };
 
