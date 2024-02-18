d8b69d0e78e84f3fd8ae37b7bfe341192a330172
Ruben Perez <rubenperez038@gmail.com>
2024-02-18 12:48:14 +0100

    Bumped copyright to 2024

--- tools/scripts/build_windows_local.bat
+++ tools/scripts/build_windows_local.bat
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright (c) 2019-2023 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+@REM Copyright (c) 2019-2024 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
 @REM
 @REM Distributed under the Boost Software License, Version 1.0. (See accompanying
 @REM file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
