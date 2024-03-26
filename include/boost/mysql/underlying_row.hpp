8c904f406d0074fa26fc460275d655810e9b5c87
Ruben Perez <rubenperez038@gmail.com>
2024-03-26 16:03:27 +0100

    underlying_row reference docs

--- include/boost/mysql/underlying_row.hpp
+++ include/boost/mysql/underlying_row.hpp
@@ -13,11 +13,26 @@
 namespace boost {
 namespace mysql {
 
-// TODO: do we want this header?
-// TODO: document
-
-template <class StaticRow>
-using underlying_row_t = detail::underlying_row_t<StaticRow>;
+/**
+ * \brief Type trait to retrieve the underlying row type.
+ * \details
+ * Given an input type `T` satisfying the `StaticRow` concept,
+ * this trait is an alias for its underlying row type. It is defined as follows:
+ * \n
+ * \li If `T` is a marker type, like \ref pfr_by_name `pfr_by_name<U>`, `underlying_row_t`
+ *     is an alias for the marker's inner type `U`.
+ * \li If `T` is not a marker type (e.g. it's a Boost.Describe struct or a `std::tuple`),
+ *     `underlying_row_t` is an alias for `T`.
+ *
+ * \ref static_results::rows uses this trait to determine its return type.
+ */
+template <BOOST_MYSQL_STATIC_ROW StaticRow>
+using underlying_row_t =
+#ifdef BOOST_MYSQL_DOXYGEN
+    __see_below__
+#else
+    detail::underlying_row_t<StaticRow>;
+#endif
 
 }  // namespace mysql
 }  // namespace boost
