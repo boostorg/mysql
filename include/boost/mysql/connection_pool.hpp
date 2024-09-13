64edd331f420eeb744f07af8e3c67a86567dd424
Ruben Perez <rubenperez038@gmail.com>
2024-09-13 19:13:49 +0200

    Add comment on get_executor

--- include/boost/mysql/connection_pool.hpp
+++ include/boost/mysql/connection_pool.hpp
@@ -504,6 +504,8 @@ public:
      * \details
      * Returns the pool executor passed to the constructor, as per
      * \ref pool_executor_params::pool_executor.
+     * This is the case even when using \ref pool_params::thread_safe:
+     * the internal strand created in this case is never exposed.
      *
      * \par Exception safety
      * No-throw guarantee.
