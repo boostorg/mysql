c8bb684eb7e04c62d4c9cb730364373eeaa7d433
Ruben Perez <rubenperez038@gmail.com>
2024-09-17 19:23:23 +0200

    Failing test for cancel get_connection

---
+++ test/thread_safety/connection_pool_cancel_get_connection.cpp
@@ -0,0 +1,182 @@
+//
+// Copyright (c) 2019-2024 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/mysql/any_connection.hpp>
+#include <boost/mysql/client_errc.hpp>
+#include <boost/mysql/connect_params.hpp>
+#include <boost/mysql/connection_pool.hpp>
+#include <boost/mysql/diagnostics.hpp>
+#include <boost/mysql/error_code.hpp>
+#include <boost/mysql/pool_params.hpp>
+#include <boost/mysql/results.hpp>
+#include <boost/mysql/ssl_mode.hpp>
+#include <boost/mysql/string_view.hpp>
+
+#include <boost/asio/any_io_executor.hpp>
+#include <boost/asio/cancel_after.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/thread_pool.hpp>
+
+#include <atomic>
+#include <chrono>
+#include <cstddef>
+#include <iostream>
+
+using boost::mysql::error_code;
+namespace mysql = boost::mysql;
+namespace asio = boost::asio;
+
+namespace {
+
+static constexpr int num_parallel = 100;
+static constexpr int total = num_parallel * 20;
+
+static void check_ec(error_code ec, mysql::diagnostics& diag)
+{
+    if (ec)
+    {
+        std::cerr << ec << ", " << diag.server_message() << std::endl;
+        exit(1);
+    }
+}
+
+class coordinator
+{
+    std::atomic_int remaining_queries_{total};
+    std::atomic_int outstanding_tasks_{num_parallel};
+    mysql::connection_pool* pool_{};
+
+public:
+    coordinator(mysql::connection_pool& pool) : pool_(&pool) {}
+    void on_finish()
+    {
+        if (--outstanding_tasks_ == 0)
+            pool_->cancel();
+    }
+    bool on_loop_finish() { return --remaining_queries_ > 0; }
+};
+
+class task
+{
+    enum class state_t
+    {
+        initial,
+        after_get_connection,
+        after_execute
+    };
+
+    mysql::connection_pool* pool_;
+    mysql::results r_;
+    mysql::diagnostics diag_;
+    coordinator* coord_{};
+    mysql::pooled_connection conn_;
+    state_t state_{state_t::initial};
+    asio::any_io_executor base_ex_;
+
+public:
+    task(mysql::connection_pool& pool, coordinator& coord, asio::any_io_executor base_ex)
+        : pool_(&pool), coord_(&coord), base_ex_(std::move(base_ex))
+    {
+    }
+
+    void resume(error_code ec = {})
+    {
+        // Error checking
+
+        switch (state_)
+        {
+            while (true)
+            {
+            case state_t::initial:
+                state_ = state_t::after_get_connection;
+                pool_->async_get_connection(
+                    diag_,
+                    asio::cancel_after(
+                        std::chrono::microseconds(1),  // make it likely to get some cancellations
+                        [this](error_code ec, mysql::pooled_connection c) {
+                            conn_ = std::move(c);
+                            resume(ec);
+                        }
+                    )
+                );
+                return;
+
+            case state_t::after_get_connection:
+                if (ec == boost::mysql::client_errc::cancelled)
+                {
+                    state_ = state_t::initial;
+                    continue;
+                }
+                check_ec(ec, diag_);
+
+                state_ = state_t::after_execute;
+                conn_->async_execute("SELECT 1", r_, diag_, [this](error_code ec) { resume(ec); });
+                return;
+
+            case state_t::after_execute:
+
+                check_ec(ec, diag_);
+                conn_ = boost::mysql::pooled_connection();
+
+                if (!coord_->on_loop_finish())
+                {
+                    coord_->on_finish();
+                    return;
+                }
+            }
+        }
+    }
+};
+
+void run(const char* hostname)
+{
+    // Setup
+    asio::thread_pool ctx(8);
+    mysql::pool_params params;
+    params.server_address.emplace_host_and_port(hostname);
+    params.username = "integ_user";
+    params.password = "integ_password";
+    params.max_size = num_parallel - 10;    // create some contention
+    params.ssl = mysql::ssl_mode::require;  // double check sharing SSL contexts is OK
+    params.thread_safe = true;
+
+    mysql::connection_pool pool(ctx, std::move(params));
+
+    pool.async_run(asio::detached);
+
+    std::vector<task> conns;
+    coordinator coord(pool);
+
+    // Create connections
+    for (std::size_t i = 0; i < num_parallel; ++i)
+        conns.emplace_back(pool, coord, ctx.get_executor());
+
+    // Launch
+    for (auto& conn : conns)
+        conn.resume(error_code());
+
+    // Run
+    ctx.join();
+}
+
+void usage(const char* progname)
+{
+    std::cerr << "Usage: " << progname << " <hostname>\n";
+    exit(1);
+}
+
+}  // namespace
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        usage(argv[0]);
+    }
+
+    run(argv[1]);
+}
