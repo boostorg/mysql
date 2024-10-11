073d54e7e5f2e7b501a0b7c373ee992f173ab7f6
Ruben Perez <rubenperez038@gmail.com>
2024-10-11 18:12:34 +0200

    with_params tutorial

--- example/1_tutorial/3_with_params.cpp
+++ example/1_tutorial/3_with_params.cpp
@@ -16,41 +16,38 @@
 #include <boost/mysql/any_connection.hpp>
 #include <boost/mysql/error_with_diagnostics.hpp>
 #include <boost/mysql/results.hpp>
+#include <boost/mysql/with_params.hpp>
 
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/this_coro.hpp>
 
+#include <cstdint>
 #include <exception>
 #include <iostream>
+#include <string>
 
 namespace mysql = boost::mysql;
 namespace asio = boost::asio;
 
 /**
- * This example is analogous to the synchronous tutorial, but uses async functions
- * with C++20 coroutines, instead. It uses the 'boost_mysql_examples' database.
- * You can get this database by running db_setup.sql.
- *
- * This function implements the main coroutine.
- * It must have a return type of boost::asio::awaitable<T>.
- * Our coroutine does not communicate any result back, so T=void.
- *
- * The coroutine will suspend every time we call one of the asynchronous functions, saving
- * all information it needs for resuming. When the asynchronous operation completes,
- * the coroutine will resume in the point it was left.
- * We use the same program structure as in the sync world, replacing
- * sync functions by their async equivalents and adding co_await in front of them.
+ * This example shows how to issue queries with parameters containing
+ * untrusted input securely. Given an employee ID, it prints their first name.
+ * The example builds on the previous async tutorial.
  */
-asio::awaitable<void> coro_main(std::string server_hostname, std::string username, std::string password)
+asio::awaitable<void> coro_main(
+    std::string server_hostname,
+    std::string username,
+    std::string password,
+    std::int64_t employee_id
+)
 {
     // Represents a connection to the MySQL server.
     // The connection will use the same executor as the coroutine
     mysql::any_connection conn(co_await asio::this_coro::executor);
 
     // The hostname, username, password and database to use.
-    // TLS is used by default.
     mysql::connect_params params{
         .server_address = mysql::host_and_port{std::move(server_hostname)},
         .username = std::move(username),
@@ -61,13 +58,26 @@ asio::awaitable<void> coro_main(std::string server_hostname, std::string usernam
     // Connect to the server
     co_await conn.async_connect(params);
 
-    // Issue the SQL query to the server
-    const char* sql = "SELECT 'Hello world!'";
+    // Execute the query with the given parameters. When executed, with_params
+    // expands the given query string template and sends it to the server for execution.
+    // {} are placeholders, as in std::format. Values are escaped as required to prevent
+    // SQL injection.
     mysql::results result;
-    co_await conn.async_execute(sql, result);
+    co_await conn.async_execute(
+        mysql::with_params("SELECT first_name FROM employee WHERE id = {}", employee_id),
+        result
+    );
 
-    // Print the first field in the first row
-    std::cout << result.rows().at(0).at(0) << std::endl;
+    // Did we find an employee with that ID?
+    if (result.rows().empty())
+    {
+        std::cout << "Employee not found" << std::endl;
+    }
+    else
+    {
+        // Print the first field in the first row
+        std::cout << "Employee's name is: " << result.rows().at(0).at(0) << std::endl;
+    }
 
     // Close the connection
     co_await conn.async_close();
@@ -75,9 +85,9 @@ asio::awaitable<void> coro_main(std::string server_hostname, std::string usernam
 
 void main_impl(int argc, char** argv)
 {
-    if (argc != 4)
+    if (argc != 5)
     {
-        std::cerr << "Usage: " << argv[0] << " <username> <password> <server-hostname>\n";
+        std::cerr << "Usage: " << argv[0] << " <username> <password> <server-hostname> <employee-id>\n";
         exit(1);
     }
 
@@ -89,7 +99,7 @@ void main_impl(int argc, char** argv)
     // Calling co_spawn enqueues the coroutine for execution.
     asio::co_spawn(
         ctx,
-        [argv] { return coro_main(argv[3], argv[1], argv[2]); },
+        [argv] { return coro_main(argv[3], argv[1], argv[2], std::stoi(argv[4])); },
         // If any exception is thrown in the coroutine body, rethrow it.
         [](std::exception_ptr ptr) {
             if (ptr)
