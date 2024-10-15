9d409e62ae412db8cf67f11ebbb6f1a1107acdc5
Ruben Perez <rubenperez038@gmail.com>
2024-10-15 18:58:35 +0200

    close_statement tests

--- test/unit/test/sansio/close_statement.cpp
+++ test/unit/test/sansio/close_statement.cpp
@@ -11,6 +11,7 @@
 #include <boost/mysql/statement.hpp>
 
 #include <boost/mysql/impl/internal/sansio/close_statement.hpp>
+#include <boost/mysql/impl/internal/sansio/connection_status.hpp>
 #include <boost/mysql/impl/internal/sansio/run_pipeline.hpp>
 
 #include <boost/test/unit_test.hpp>
@@ -23,6 +24,7 @@
 #include "test_unit/create_frame.hpp"
 #include "test_unit/create_ok.hpp"
 #include "test_unit/create_ok_frame.hpp"
+#include "test_unit/printing.hpp"
 
 using namespace boost::mysql::test;
 using namespace boost::mysql;
@@ -95,6 +97,31 @@ BOOST_AUTO_TEST_CASE(error_response)
                          .message("my_message")
                          .build_frame())  // Error response
         .check(fix, common_server_errc::er_bad_db_error, create_server_diag("my_message"));
+}
+
+BOOST_AUTO_TEST_CASE(error_invalid_connection_state)
+{
+    struct
+    {
+        detail::connection_status status;
+        error_code expected_err;
+    } test_cases[] = {
+        {detail::connection_status::not_connected,             client_errc::not_connected            },
+        {detail::connection_status::engaged_in_multi_function, client_errc::engaged_in_multi_function},
+    };
+
+    for (const auto& tc : test_cases)
+    {
+        BOOST_TEST_CONTEXT(tc.status)
+        {
+            // Setup
+            fixture fix;
+            fix.st.status = tc.status;
+
+            // Run the algo
+            algo_test().check(fix, tc.expected_err);
+        }
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
