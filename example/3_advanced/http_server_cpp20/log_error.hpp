2c6e4be8000743411b112302f0f609ed2769c5cb
Ruben Perez <rubenperez038@gmail.com>
2024-12-15 16:15:38 +0100

    Update log_error includes

--- example/3_advanced/http_server_cpp20/log_error.hpp
+++ example/3_advanced/http_server_cpp20/log_error.hpp
@@ -5,22 +5,21 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef BOOST_MYSQL_EXAMPLE_3_ADVANCED_CONNECTION_POOL_LOG_ERROR_HPP
-#define BOOST_MYSQL_EXAMPLE_3_ADVANCED_CONNECTION_POOL_LOG_ERROR_HPP
+#ifndef BOOST_MYSQL_EXAMPLE_3_ADVANCED_HTTP_SERVER_CPP20_LOG_ERROR_HPP
+#define BOOST_MYSQL_EXAMPLE_3_ADVANCED_HTTP_SERVER_CPP20_LOG_ERROR_HPP
 
 //[example_connection_pool_log_error_hpp
 //
 // File: log_error.hpp
 //
-
-#include <iostream>
-#include <mutex>
-
 // Helper function to safely write diagnostics to std::cerr.
 // Since we're in a multi-threaded environment, directly writing to std::cerr
 // can lead to interleaved output, so we should synchronize calls with a mutex.
 // This function is only called in rare cases (e.g. unhandled exceptions),
 // so we can afford the synchronization overhead.
+
+#include <iostream>
+#include <mutex>
 
 namespace orders {
 
