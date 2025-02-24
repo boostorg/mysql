e988c9ad9e94766fca3845a63ef84a315ffc3513
Anarthal (Rubén Pérez) <34971811+anarthal@users.noreply.github.com>
2025-02-24 22:11:35 +0100

    Added docs on using asio::cancel_after with pool_params::thread_safe
    close #402

--- test/integration/test/snippets/connection_pool.cpp
+++ test/integration/test/snippets/connection_pool.cpp
@@ -13,11 +13,16 @@
 #include <boost/mysql/with_diagnostics.hpp>
 
 #include <boost/asio/awaitable.hpp>
+#include <boost/asio/bind_executor.hpp>
 #include <boost/asio/cancel_after.hpp>
+#include <boost/asio/co_spawn.hpp>
 #include <boost/asio/detached.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/strand.hpp>
 #include <boost/asio/thread_pool.hpp>
 #include <boost/asio/use_future.hpp>
+#include <boost/config.hpp>
+#include <boost/system/error_code.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <chrono>
@@ -26,8 +31,9 @@
 #include "test_integration/run_coro.hpp"
 #include "test_integration/snippets/credentials.hpp"
 
-using namespace boost::mysql;
-using namespace boost::mysql::test;
+namespace asio = boost::asio;
+namespace mysql = boost::mysql;
+using namespace mysql::test;
 
 namespace {
 
@@ -36,15 +42,15 @@ namespace {
 // Use connection pools for functions that will be called
 // repeatedly during the application lifetime.
 // An HTTP server handler function is a good candidate.
-boost::asio::awaitable<std::int64_t> get_num_employees(boost::mysql::connection_pool& pool)
+asio::awaitable<std::int64_t> get_num_employees(mysql::connection_pool& pool)
 {
     // Get a fresh connection from the pool.
     // pooled_connection is a proxy to an any_connection object.
-    boost::mysql::pooled_connection conn = co_await pool.async_get_connection();
+    mysql::pooled_connection conn = co_await pool.async_get_connection();
 
     // Use pooled_connection::operator-> to access the underlying any_connection.
     // Let's use the connection
-    results result;
+    mysql::results result;
     co_await conn->async_execute("SELECT COUNT(*) FROM employee", result);
     co_return result.rows().at(0).at(0).as_int64();
 
@@ -52,16 +58,16 @@ boost::asio::awaitable<std::int64_t> get_num_employees(boost::mysql::connection_
 }
 //]
 
-boost::asio::awaitable<void> return_without_reset(boost::mysql::connection_pool& pool)
+asio::awaitable<void> return_without_reset(mysql::connection_pool& pool)
 {
     //[connection_pool_return_without_reset
     // Get a connection from the pool
-    boost::mysql::pooled_connection conn = co_await pool.async_get_connection();
+    mysql::pooled_connection conn = co_await pool.async_get_connection();
 
     // Use the connection in a way that doesn't mutate session state.
     // We're not setting variables, preparing statements or starting transactions,
     // so it's safe to skip reset
-    boost::mysql::results result;
+    mysql::results result;
     co_await conn->async_execute("SELECT COUNT(*) FROM employee", result);
 
     // Explicitly return the connection to the pool, skipping reset
@@ -69,15 +75,79 @@ boost::asio::awaitable<void> return_without_reset(boost::mysql::connection_pool&
     //]
 }
 
-boost::asio::awaitable<void> apply_timeout(boost::mysql::connection_pool& pool)
+asio::awaitable<void> apply_timeout(mysql::connection_pool& pool)
 {
     //[connection_pool_apply_timeout
     // Get a connection from the pool, but don't wait more than 5 seconds
-    auto conn = co_await pool.async_get_connection(boost::asio::cancel_after(std::chrono::seconds(5)));
+    auto conn = co_await pool.async_get_connection(asio::cancel_after(std::chrono::seconds(5)));
     //]
 
     conn.return_without_reset();
 }
+
+//[connection_pool_thread_safe_use
+// A function that handles a user session in a server
+asio::awaitable<void> handle_session(mysql::connection_pool& pool)
+{
+    // CAUTION: asio::cancel_after creates a timer that is *not* part of the pool's state.
+    // The timer is not protected by the pool's strand.
+    // This coroutine must be run within a strand for this to be safe
+    using namespace std::chrono_literals;
+    co_await pool.async_get_connection(asio::cancel_after(30s));
+
+    // Use the connection
+}
+//]
+#endif
+
+#ifndef BOOST_NO_CXX14_INITIALIZED_LAMBDA_CAPTURES
+//[connection_pool_thread_safe_callbacks
+// Holds per-session state
+class session_handler : public std::enable_shared_from_this<session_handler>
+{
+    // The connection pool
+    mysql::connection_pool& pool_;
+
+    // A strand object, unique to this session
+    asio::strand<asio::any_io_executor> strand_;
+
+public:
+    // pool.get_executor() points to the execution context that was used
+    // to create the pool, and never to the pool's internal strand
+    session_handler(mysql::connection_pool& pool)
+        : pool_(pool), strand_(asio::make_strand(pool.get_executor()))
+    {
+    }
+
+    void start()
+    {
+        // Enters the strand. The passed function will be executed through the strand.
+        // If the initiation is run outside the strand, a race condition will occur.
+        asio::dispatch(asio::bind_executor(strand_, [self = shared_from_this()] { self->get_connection(); }));
+    }
+
+    void get_connection()
+    {
+        // This function will run within the strand. Binding the passed callback to
+        // the strand will make async_get_connection run it within the strand, too.
+        pool_.async_get_connection(asio::cancel_after(
+            std::chrono::seconds(30),
+            asio::bind_executor(
+                strand_,
+                [self = shared_from_this()](boost::system::error_code, mysql::pooled_connection) {
+                    // Use the connection as required
+                }
+            )
+        ));
+    }
+};
+
+void handle_session_v2(mysql::connection_pool& pool)
+{
+    // Start the callback chain
+    std::make_shared<session_handler>(pool)->start();
+}
+//]
 #endif
 
 BOOST_AUTO_TEST_CASE(section_connection_pool)
@@ -90,39 +160,39 @@ BOOST_AUTO_TEST_CASE(section_connection_pool)
         // You must specify enough information to establish a connection,
         // including the server address and credentials.
         // You can configure a lot of other things, like pool limits
-        boost::mysql::pool_params params;
+        mysql::pool_params params;
         params.server_address.emplace_host_and_port(server_hostname);
         params.username = mysql_username;
         params.password = mysql_password;
         params.database = "boost_mysql_examples";
 
         // The I/O context, required by all I/O operations
-        boost::asio::io_context ctx;
+        asio::io_context ctx;
 
         // Construct a pool of connections. The context will be used internally
         // to create the connections and other I/O objects
-        boost::mysql::connection_pool pool(ctx, std::move(params));
+        mysql::connection_pool pool(ctx, std::move(params));
 
         // You need to call async_run on the pool before doing anything useful with it.
         // async_run creates connections and keeps them healthy. It must be called
         // only once per pool.
         // The detached completion token means that we don't want to be notified when
         // the operation ends. It's similar to a no-op callback.
-        pool.async_run(boost::asio::detached);
+        pool.async_run(asio::detached);
         //]
 
 #ifdef BOOST_ASIO_HAS_CO_AWAIT
-        run_coro(ctx, [&pool]() -> boost::asio::awaitable<void> {
+        run_coro(ctx, [&pool]() -> asio::awaitable<void> {
             co_await get_num_employees(pool);
             pool.cancel();
         });
 #endif
     }
     {
-        boost::asio::io_context ctx;
+        asio::io_context ctx;
 
         //[connection_pool_configure_size
-        boost::mysql::pool_params params;
+        mysql::pool_params params;
 
         // Set the usual params
         params.server_address.emplace_host_and_port(server_hostname);
@@ -134,12 +204,12 @@ BOOST_AUTO_TEST_CASE(section_connection_pool)
         params.initial_size = 10;
         params.max_size = 1000;
 
-        boost::mysql::connection_pool pool(ctx, std::move(params));
+        mysql::connection_pool pool(ctx, std::move(params));
         //]
 
 #ifdef BOOST_ASIO_HAS_CO_AWAIT
-        pool.async_run(boost::asio::detached);
-        run_coro(ctx, [&pool]() -> boost::asio::awaitable<void> {
+        pool.async_run(asio::detached);
+        run_coro(ctx, [&pool]() -> asio::awaitable<void> {
             co_await return_without_reset(pool);
             co_await apply_timeout(pool);
             pool.cancel();
@@ -147,12 +217,13 @@ BOOST_AUTO_TEST_CASE(section_connection_pool)
 #endif
     }
     {
-        //[connection_pool_thread_safe
-        // The I/O context, required by all I/O operations
-        boost::asio::io_context ctx;
+        //[connection_pool_thread_safe_create
+        // The I/O context, required by all I/O operations.
+        // This is like an io_context, but with 5 threads running it.
+        asio::thread_pool ctx(5);
 
         // The usual pool configuration params
-        boost::mysql::pool_params params;
+        mysql::pool_params params;
         params.server_address.emplace_host_and_port(server_hostname);
         params.username = mysql_username;
         params.password = mysql_password;
@@ -160,12 +231,31 @@ BOOST_AUTO_TEST_CASE(section_connection_pool)
         params.thread_safe = true;  // enable thread safety
 
         // Construct a thread-safe pool
-        boost::mysql::connection_pool pool(ctx, std::move(params));
+        mysql::connection_pool pool(ctx, std::move(params));
+        pool.async_run(asio::detached);
 
         // We can now pass a reference to pool to other threads,
         // and call async_get_connection concurrently without problem.
         // Individual connections are still not thread-safe.
         //]
+
+#ifdef BOOST_ASIO_HAS_CO_AWAIT
+        //[connection_pool_thread_safe_spawn
+        // OK: the entire coroutine runs within a strand.
+        // In a typical server setup, each request usually gets its own strand,
+        // so it can run in parallel with other requests.
+        asio::co_spawn(
+            asio::make_strand(ctx),  // If we removed this make_strand, we would have a race condition
+            [&pool] { return handle_session(pool); },
+            asio::detached
+        );
+        //]
+#endif
+#ifndef BOOST_NO_CXX14_INITIALIZED_LAMBDA_CAPTURES
+        handle_session_v2(pool);
+#endif
+        pool.cancel();
+        ctx.join();
     }
 }
 
