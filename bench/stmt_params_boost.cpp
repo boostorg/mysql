646caf84f0f182548364c00c360d4cd3b9bb6af3
Ruben Perez <rubenperez038@gmail.com>
2025-03-27 17:43:15 +0100

    stmt params bench v1

---
+++ bench/stmt_params_boost.cpp
@@ -0,0 +1,99 @@
+#include <boost/mysql/any_connection.hpp>
+#include <boost/mysql/blob_view.hpp>
+#include <boost/mysql/connect_params.hpp>
+#include <boost/mysql/date.hpp>
+#include <boost/mysql/datetime.hpp>
+#include <boost/mysql/execution_state.hpp>
+#include <boost/mysql/results.hpp>
+#include <boost/mysql/ssl_mode.hpp>
+#include <boost/mysql/time.hpp>
+
+#include <boost/asio/io_context.hpp>
+
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string_view>
+#include <vector>
+
+using namespace std;
+
+namespace asio = boost::asio;
+namespace mysql = boost::mysql;
+
+int main()
+{
+    using namespace std::chrono;
+
+    asio::io_context ctx;
+    mysql::any_connection conn(ctx);
+    mysql::connect_params params;
+    params.server_address.emplace_unix_path("/var/run/mysqld/mysqld.sock");
+    params.username = "root";
+    params.password = "";
+    params.database = "mytest";
+    params.ssl = mysql::ssl_mode::disable;
+    conn.connect(params);
+
+    // Prepare stmt
+    auto stmt = conn.prepare_statement(
+        "SELECT id FROM test_data WHERE id = 1 AND s8_0 = ? AND u8_0 = ? AND s16_0 = ? AND u16_0 = ? AND "
+        "s32_0 = ? AND u32_0 = ? AND s64_0 = ? AND u64_0 = ? AND s1_0 = ? AND s2_0 = ? AND b1_0 = ? AND "
+        "b2_0 = ? AND flt_0 = ? AND dbl_0 = ? AND dt_0 = ? AND dtime_0 = ? AND t_0 = ?"
+    );
+
+    // Statement params
+    signed char s8 = 64;
+    unsigned char u8 = 172;
+    short s16 = -129;
+    unsigned short u16 = 0xfe21;
+    int s32 = 42;
+    unsigned u32 = 0xfe8173;
+    long long s64 = -1;
+    unsigned long long u64 = 98302402;
+    std::string s1(200, 'a');
+    std::string s2(36000, 'b');
+    std::vector<unsigned char> b1(200, 5);
+    std::vector<unsigned char> b2(35000, 7);
+    float flt = 3.14e10;
+    double dbl = 7.1e-150;
+    mysql::date dt(2010, 6, 20);
+    mysql::datetime dtime(2020, 3, 21, 10, 40, 10, 123456);
+    mysql::time t = hours(126) + minutes(18) + seconds(40) + microseconds(123456);
+
+    // Output results
+    mysql::results r;
+
+    auto tbegin = std::chrono::steady_clock::now();
+    for (int i = 0; i < 10000; ++i)
+    {
+        conn.execute(
+            stmt.bind(
+                s8,
+                u8,
+                s16,
+                u16,
+                s32,
+                u32,
+                s64,
+                u64,
+                string_view(s1),
+                string_view(s2),
+                mysql::blob_view(b1),
+                mysql::blob_view(b2),
+                flt,
+                dbl,
+                dt,
+                dtime,
+                t
+            ),
+            r
+        );
+    }
+    auto tend = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(tend - tbegin).count() << std::endl;
+}
---
+++ bench/stmt_params_libmysqlclient.cpp
@@ -0,0 +1,222 @@
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <mysql/field_types.h>
+#include <mysql/mysql.h>
+#include <mysql/mysql_com.h>
+#include <mysql/mysql_time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    if (mysql_library_init(0, NULL, NULL))
+    {
+        fprintf(stderr, "could not initialize MySQL client library\n");
+        exit(1);
+    }
+    MYSQL* con = mysql_init(NULL);
+    if (con == NULL)
+    {
+        fprintf(stderr, "Error initializing connection: %s\n", mysql_error(con));
+        exit(1);
+    }
+
+    unsigned mode = SSL_MODE_DISABLED;
+    if (mysql_options(con, MYSQL_OPT_SSL_MODE, &mode))
+    {
+        fprintf(stderr, "Error in mysql_options: %s\n", mysql_error(con));
+        exit(1);
+    }
+
+    if (mysql_real_connect(con, NULL, "root", "", "mytest", 0, "/var/run/mysqld/mysqld.sock", 0) == NULL)
+    {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+
+    // Statement params
+    signed char s8 = 64;
+    unsigned char u8 = 172;
+    short s16 = -129;
+    unsigned short u16 = 0xfe21;
+    int s32 = 42;
+    unsigned u32 = 0xfe8173;
+    long long s64 = -1;
+    unsigned long long u64 = 98302402;
+    std::string s1(200, 'a');
+    std::string s2(36000, 'b');
+    std::vector<unsigned char> b1(200, 5);
+    std::vector<unsigned char> b2(35000, 7);
+    float flt = 3.14e10;
+    double dbl = 7.1e-150;
+    MYSQL_TIME dt{}, dtime{}, t{};
+
+    dt.year = 2010;
+    dt.month = 6;
+    dt.day = 20;
+
+    dtime.year = 2020;
+    dtime.month = 3;
+    dtime.day = 21;
+    dtime.hour = 10;
+    dtime.minute = 40;
+    dtime.second = 10;
+    dtime.second_part = 123456;
+
+    t.hour = 126;
+    t.minute = 18;
+    t.second = 40;
+    t.second_part = 123456;
+
+    // Prepare stmt
+    MYSQL_STMT* stmt;
+    stmt = mysql_stmt_init(con);
+    if (!stmt)
+    {
+        printf("Could not initialize statement\n");
+        exit(1);
+    }
+    constexpr const char* stmt_str =
+        "SELECT id FROM test_data WHERE id = 1 AND s8_0 = ? AND u8_0 = ? AND s16_0 = ? AND u16_0 = ? AND "
+        "s32_0 = ? AND u32_0 = ? AND s64_0 = ? AND u64_0 = ? AND s1_0 = ? AND s2_0 = ? AND b1_0 = ? AND "
+        "b2_0 = ? AND flt_0 = ? AND dbl_0 = ? AND dt_0 = ? AND dtime_0 = ? AND t_0 = ?";
+    if (mysql_stmt_prepare(stmt, stmt_str, strlen(stmt_str)))
+    {
+        fprintf(stderr, "Error preparing statement: %s\n", mysql_stmt_error(stmt));
+        exit(1);
+    }
+
+    // Prepare the bind objects
+    MYSQL_BIND in_binds[27]{};
+    in_binds[0].buffer_type = MYSQL_TYPE_TINY;
+    in_binds[0].buffer = &s8;
+    in_binds[0].buffer_length = 1;
+    in_binds[0].is_unsigned = 0;
+
+    in_binds[1].buffer_type = MYSQL_TYPE_TINY;
+    in_binds[1].buffer = &u8;
+    in_binds[1].buffer_length = 1;
+    in_binds[1].is_unsigned = 1;
+
+    in_binds[2].buffer_type = MYSQL_TYPE_SHORT;
+    in_binds[2].buffer = &s16;
+    in_binds[2].buffer_length = 2;
+    in_binds[2].is_unsigned = 0;
+
+    in_binds[3].buffer_type = MYSQL_TYPE_SHORT;
+    in_binds[3].buffer = &u16;
+    in_binds[3].buffer_length = 2;
+    in_binds[3].is_unsigned = 1;
+
+    in_binds[4].buffer_type = MYSQL_TYPE_LONG;
+    in_binds[4].buffer = &s32;
+    in_binds[4].buffer_length = 4;
+    in_binds[4].is_unsigned = 0;
+
+    in_binds[5].buffer_type = MYSQL_TYPE_LONG;
+    in_binds[5].buffer = &u32;
+    in_binds[5].buffer_length = 4;
+    in_binds[5].is_unsigned = 1;
+
+    in_binds[6].buffer_type = MYSQL_TYPE_LONGLONG;
+    in_binds[6].buffer = &s64;
+    in_binds[6].buffer_length = 8;
+    in_binds[6].is_unsigned = 0;
+
+    in_binds[7].buffer_type = MYSQL_TYPE_LONGLONG;
+    in_binds[7].buffer = &u64;
+    in_binds[7].buffer_length = 8;
+    in_binds[7].is_unsigned = 1;
+
+    in_binds[8].buffer_type = MYSQL_TYPE_STRING;
+    in_binds[8].buffer = s1.data();
+    in_binds[8].buffer_length = s1.size();
+
+    in_binds[9].buffer_type = MYSQL_TYPE_STRING;
+    in_binds[9].buffer = s2.data();
+    in_binds[9].buffer_length = s2.size();
+
+    in_binds[10].buffer_type = MYSQL_TYPE_BLOB;
+    in_binds[10].buffer = b1.data();
+    in_binds[10].buffer_length = b1.size();
+
+    in_binds[11].buffer_type = MYSQL_TYPE_BLOB;
+    in_binds[11].buffer = b2.data();
+    in_binds[11].buffer_length = b2.size();
+
+    in_binds[12].buffer_type = MYSQL_TYPE_FLOAT;
+    in_binds[12].buffer = &flt;
+    in_binds[12].buffer_length = 4;
+
+    in_binds[13].buffer_type = MYSQL_TYPE_DOUBLE;
+    in_binds[13].buffer = &dbl;
+    in_binds[13].buffer_length = 8;
+
+    in_binds[14].buffer_type = MYSQL_TYPE_DATE;
+    in_binds[14].buffer = &dt;
+    in_binds[14].buffer_length = sizeof(dt);
+
+    in_binds[15].buffer_type = MYSQL_TYPE_DATETIME;
+    in_binds[15].buffer = &dtime;
+    in_binds[15].buffer_length = sizeof(dtime);
+
+    in_binds[16].buffer_type = MYSQL_TYPE_TIME;
+    in_binds[16].buffer = &t;
+    in_binds[16].buffer_length = sizeof(t);
+
+    long long int out_id = 0;
+    MYSQL_BIND out_binds[1]{};
+    out_binds[0].buffer_type = MYSQL_TYPE_LONGLONG;
+    out_binds[0].buffer = &out_id;
+    out_binds[0].buffer_length = 8;
+
+    auto tbegin = std::chrono::steady_clock::now();
+    for (int i = 0; i < 10000; ++i)
+    {
+        if (mysql_stmt_bind_param(stmt, in_binds))
+        {
+            fprintf(stderr, "Error binding params: %s\n", mysql_stmt_error(stmt));
+            exit(1);
+        }
+
+        if (mysql_stmt_execute(stmt))
+        {
+            fprintf(stderr, "Error executing statement: %s\n", mysql_stmt_error(stmt));
+            exit(1);
+        }
+
+        if (mysql_stmt_bind_result(stmt, out_binds))
+        {
+            fprintf(stderr, "Error binding result: %s\n", mysql_stmt_error(stmt));
+            exit(1);
+        }
+
+        while (true)
+        {
+            auto status = mysql_stmt_fetch(stmt);
+            if (status == MYSQL_NO_DATA)
+            {
+                break;
+            }
+            else if (status == 1)
+            {
+                fprintf(stderr, "Error fetching result: %s\n", mysql_stmt_error(stmt));
+                exit(1);
+            }
+        }
+    }
+
+    auto tend = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(tend - tbegin).count() << std::endl;
+
+    mysql_stmt_close(stmt);
+    mysql_close(con);
+    exit(0);
+}
