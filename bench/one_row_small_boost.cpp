20eb3796e3ddacf81f944d5c4cad654d8dfbfb42
Ruben Perez <rubenperez038@gmail.com>
2025-03-31 17:31:07 +0200

    one row small boost cleanup

--- bench/one_row_small_boost.cpp
+++ bench/one_row_small_boost.cpp
@@ -1,3 +1,9 @@
+//
+// Copyright (c) 2019-2025 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <boost/mysql/any_connection.hpp>
 #include <boost/mysql/connect_params.hpp>
 #include <boost/mysql/execution_state.hpp>
@@ -6,23 +12,23 @@
 
 #include <boost/asio/io_context.hpp>
 
-#include <cassert>
 #include <chrono>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <stdlib.h>
 #include <string>
-
-using namespace std;
 
 namespace asio = boost::asio;
 namespace mysql = boost::mysql;
 
 int main()
 {
+    // Setup
     asio::io_context ctx;
     mysql::any_connection conn(ctx);
+    mysql::results r;
+
+    // Connect
     mysql::connect_params params;
     params.server_address.emplace_unix_path("/var/run/mysqld/mysqld.sock");
     params.username = "root";
@@ -31,24 +37,29 @@ int main()
     params.ssl = mysql::ssl_mode::disable;
     conn.connect(params);
 
-    unsigned res = 0;
-
-    // Prepare stmt
+    // Prepare the statement. Exclude the big TEXT/BLOB fields.
     auto stmt = conn.prepare_statement(
         "SELECT s8, u8, s16, u16, s32, u32, s64, u64, s1, b1, flt, dbl, dt, dtime, t "
         "FROM test_data WHERE id = 1"
     );
 
-    // Output results
-    mysql::results r;
+    // Ensure that nothing gets optimized away
+    unsigned res = 0;
 
+    // Benchmark starts here
     auto tbegin = std::chrono::steady_clock::now();
+
     for (int i = 0; i < 10000; ++i)
     {
+        // Since the rows are small, using execute is recommended
         conn.execute(stmt.bind(), r);
         res += r.rows().size();
     }
+
+    // Benchmark ends here
     auto tend = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(tend - tbegin).count() << std::endl;
-    return res == 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+
+    // We expect one row per iteration
+    return res == 10000 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
