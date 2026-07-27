908c436579859b9a00fb0f61bc59101c6b198ed8
vsoulgard <vsoulgard@gmail.com>
2026-07-27 22:40:41 +0300

    add standalone headers tests

---
+++ test/standalone/character_set.cpp
@@ -0,0 +1,8 @@
+//
+// Copyright (c) 2026 Vladislav Soulgard (vsoulgard at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/mysql/character_set.hpp>
